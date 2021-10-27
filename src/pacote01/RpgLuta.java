a0751882b0c9de62c7657c028cc1744b01c04016
alessandra1408 <alessandraoitnb@gmail.com>
2021-10-26 23:30:28 -0300

    estrutura de característica iniciada.

---
+++ src/pacote01/RpgLuta.java
@@ -0,0 +1,38 @@
+package pacote01;
+
+import java.util.Scanner;
+
+public class RpgLuta{
+
+    static int escolhaCaracteristicas(){
+        System.out.println("Seja bem vindo(a) à BATALHA FINAL!\n");
+        System.out.println("[escolha do nível de dificuldade]\n");
+        System.out.println("1, 2 ou 3");
+        Scanner level = new Scanner(System.in);
+        int level1 = level.nextInt();
+
+        System.out.println("[escolha o seu nick]\n");
+        Scanner nick = new Scanner(System.in);
+        String nick1 = nick.next();
+
+        System.out.println("[escolha o seu gênero]\n");
+        System.out.println("[Feminino]\n[Masculino]\n[Outro]\n");
+
+        Scanner gender = new Scanner(System.in);
+        String gender1 = gender.next();
+
+        System.out.println("=================");
+        System.out.println("Nível: " + level1);
+        System.out.println("Nick: " + nick1);
+        System.out.println("Gênero: " + gender1);
+        System.out.println("=================");
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        escolhaCaracteristicas();
+    }
+}
+
+
